4636756f5226571f99843c9b5bee7973183ee3fe
OlThib <oli.thibault965@gmail.com>
2023-11-23 23:46:56 -0500

    Started using the calculation avaible from the DMP

--- robot/test/UNO_MPU6050/UNO_MPU6050.ino
+++ robot/test/UNO_MPU6050/UNO_MPU6050.ino
@@ -1,122 +1,147 @@
 
 #include "I2Cdev.h"
-#include "MPU6050.h"
+#include "MPU6050_6Axis_MotionApps_V6_12.h"
+//#include "MPU6050.h"
 #include "Wire.h"
 
 
-MPU6050 accelgyro;
+//MPU6050 accelgyro;
+MPU6050 mpu;
 
-const uint8_t maxAlpha = 100;
-const float alpha = 0.01;
-const uint8_t serialDelay = 40;
-const uint16_t calibDelay = 10000;
+float alpha = 0.01;
 
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-unsigned long gyroTimer = 0, serialTimer = 0;
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 gy;         // [x, y, z]            gyro sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-int16_t numFiltering(int16_t lastVal, int16_t newVal) {
-  return (1 - alpha) * lastVal + newVal * alpha;
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
 }
 
-float calculateAngle(float lastAngle, int16_t angleVariation, uint8_t deltaT) {
-  float LSB = 16384.0;
-  float retVal = lastAngle;  
-
-  // Ignore small movements
-  if (abs(angleVariation) > 200) { retVal += float(angleVariation / LSB) * float(deltaT / (2*PI)); }
-  // Keep the angle between 0 and 360°
-  if (retVal >= 360.0) { retVal -= 360.0; } 
-  else if ( retVal < 0.0) { retVal += 360.0; }
-  return retVal;
+int16_t numFiltering(int16_t newVal, int16_t lastVal) {
+  return (1 - alpha) * lastVal + alpha * newVal;
 }
 
 void setup() {
 
   Wire.begin();
-  Serial.begin(38400);
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  Serial.begin(115200);
   delay(1000);
 
   // initialize device
   Serial.println("Initializing I2C devices...");
-  accelgyro.initialize();
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
 
   // verify connection
   Serial.println("Testing device connections...");
-  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
-  // Calib gyro offsets
-  /*accelgyro.setXGyroOffset(100);
-  accelgyro.setYGyroOffset(33);
-  accelgyro.setZGyroOffset(28);*/
-  Serial.println("Present offset configuration (ax, ay, az / gx, gy, gz): ");
-  Serial.print(accelgyro.getXAccelOffset());
-  Serial.print("\t");  // -76
-  Serial.print(accelgyro.getYAccelOffset());
-  Serial.print("\t");  // -2359
-  Serial.print(accelgyro.getZAccelOffset());
-  Serial.print("\t");  // 1688
-  Serial.print(accelgyro.getXGyroOffset());
-  Serial.print("\t");  // 0
-  Serial.print(accelgyro.getYGyroOffset());
-  Serial.print("\t");  // 0
-  Serial.print(accelgyro.getZGyroOffset());
-  Serial.print("\t");  // 0
-  Serial.print("\n");
+    // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
   
-  Serial.println("Now mesuring drift, do not move for 10 seconds...");
-  gyroTimer = millis();
-  bool calibDone = false;
-  accelgyro.setXGyroOffset(0);
-  accelgyro.setYGyroOffset(0);
-  accelgyro.setZGyroOffset(0);
-  while (!calibDone) {
-    static int16_t accX = 0, accY = 0, accZ = 0, gyrX = 0, gyrY = 0, gyrZ = 0;
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    accX = numFiltering(accX, ax);
-    accY = numFiltering(accY, ay);
-    accZ = numFiltering(accZ, az);
-    gyrX = numFiltering(gyrX, gx);
-    gyrY = numFiltering(gyrY, gy);
-    gyrZ = numFiltering(gyrZ, gz);
-    if (millis() - gyroTimer >= calibDelay) {
-      calibDone = true;
-      Serial.println("Done. Drift measured on gyroscope: "+ String(gyrX) + ", " + String(gyrY) + ", " + String(gyrZ));
-      accelgyro.setXGyroOffset(-gyrX/3);
-      accelgyro.setYGyroOffset(-gyrY/3);
-      accelgyro.setZGyroOffset(-gyrZ/3);
-    }
+  // supply your own gyro offsets here, scaled for min sensitivity
+  
+  mpu.setXGyroOffset(106);
+  mpu.setYGyroOffset(32);
+  mpu.setZGyroOffset(36);
+  mpu.setXAccelOffset(-584);
+  mpu.setYAccelOffset(-830);
+  mpu.setZAccelOffset(1200);
+  
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    Serial.println();
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
   }
-  Serial.println("Setup done, starting main");
-  delay(1000);  
+
 }
 
 void loop() {
-  static int16_t accX = 0, accY = 0, accZ = 0, gyrX = 0, gyrY = 0, gyrZ = 0;
-  static float pitch = 0.0, yaw = 0.0, roll = 0.0;
-  // read raw accel/gyro measurements from device
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  // these methods (and a few others) are also available
-  //accelgyro.getAcceleration(&ax, &ay, &az);
-  //accelgyro.getRotation(&gx, &gy, &gz);
-  accX = numFiltering(accX, ax);
-  accY = numFiltering(accY, ay);
-  accZ = numFiltering(accZ, az);
-  gyrX = numFiltering(gyrX, gx);
-  gyrY = numFiltering(gyrY, gy);
-  gyrZ = numFiltering(gyrZ, gz);
-
-
-  if (millis() - serialTimer >= serialDelay) {
-    serialTimer = millis();
-    yaw = calculateAngle(yaw, gyrZ, serialDelay);
-    Serial.print(gyrX);
+  static int16_t ax = 0, ay = 0, az = 0;
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+    ax = numFiltering(aaWorld.x, ax);
+    ay = numFiltering(aaWorld.y, ay);
+    az = numFiltering(aaWorld.z - 8140, az);
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180 / M_PI);
     Serial.print("\t");
-    Serial.print(gyrY);
+    Serial.print(ypr[1] * 180 / M_PI);
     Serial.print("\t");
-    Serial.print(gyrZ);
+    Serial.print(ypr[2] * 180 / M_PI);    
+    Serial.print("\t| aworld\t");
+    Serial.print(ax);
     Serial.print("\t");
-    Serial.println(yaw);
+    Serial.print(ay);
+    Serial.print("\t");
+    Serial.print(az);
+    Serial.println();
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
   }
+
 }
