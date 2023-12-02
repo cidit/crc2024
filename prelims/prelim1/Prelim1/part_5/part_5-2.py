0369f22fe4834dcfb673c0376d776eb68dbe8a34
OlThib <oli.thibault965@gmail.com>
2023-12-01 22:06:17 -0500

    Fixed something stupid about the code...

--- prelims/prelim1/Prelim1/part_5/part_5-2.py
+++ prelims/prelim1/Prelim1/part_5/part_5-2.py
@@ -1,45 +1,23 @@
-# For your imports look at the rulebook to see what is allowed
-# Pour les import permis allez vous référer au livre des règlements
-"""part_5.py
-This is the template file for the part 4 of the Prelim 1.
-Ceci est le fichier template pour la partie 4 du Prelim 1.
-"""
-
-### Don't forget to write your own tests with pytest ###
-### Noubliez pas d'aller écrire vos propres tests ave pytest ###
 def part_5(text: str):
-    """
-    Print the 5 given words in an interesting format.
 
-    Parameters:
-        text (str): The input string containing 5 words.
-
-    Returns:
-        what you want.
-    """
-    ### You code goes here ###
-    ### Votre code va ici ###
     words = text.split()
     rowNbr = len(words) * 3
     longest_lenght = max(map(lambda word: len(word), words))
+    if longest_lenght == 0 :
+        return None
+    
     words = list(map(lambda word: word.ljust(longest_lenght), words))
     rows = ["" for row in range(rowNbr)]
-    for line in range(rowNbr):
-        if line % 3 == 0:    
-            for idx, char in enumerate(words[int(line/3)]): 
-                if idx % 2 == 0:
-                    rows[line] += char + " "
- 
-        elif line % 3 == 1 :
-            for idx, char in enumerate(words[int(line/3)]): 
-                if idx % 2 != 0:
-                    rows[line] += " " + char
+    for line in range(0, rowNbr, 3):
+        for idx, char in enumerate(words[int(line/3)]):
+            if idx % 2 == 0:
+                rows[line] += char + " "
+            else :
+                rows[line + 1] += " " + char 
+                                   
     cascade = map(lambda row: row, rows)
     return list(cascade)
     
-
-# You can make you own test with simple print but you will need to make pytest to have all the points
-# Vous pouvez faire des tests avec print simple mais il faut utiliser pytest pour avoir tous les points
 
 def print_cascade(cascade) -> None:
     for level in cascade:
@@ -47,6 +25,6 @@ def print_cascade(cascade) -> None:
 
 
 print_cascade(part_5('Les volcants uniques du pacifique')) 
-# # Expected output: ???
+
 
 
