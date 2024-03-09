2022b8d9aca2f93a4cdaad0afe65795ad9f8ac1b
Guillaume1234566436 <158013035+Guillaume1234566436@users.noreply.github.com>
2024-03-09 18:02:41 -0500

    Co-authored-by: ondroskotch <ondroskotch@users.noreply.github.com>

--- web/main.js
+++ web/main.js
@@ -31,11 +31,51 @@ Féliximage.onclick=(event) => {
 const Guillaumeimage = document.getElementById("Guillaume-image-dropdown")
 let isopenGuillaume = false
 Guillaumeimage.onclick=(event) => {
-    event.stopPropagation()Guillaume
+    event.stopPropagation()
     isopenGuillaume = !isopenGuillaume
     const Guillaumedropdown= document.getElementById("Guillaume-dropdown")
 
     Guillaumedropdown.style.display=isopenGuillaume?"block":"none"
+}
+
+const Léoimage = document.getElementById("Léo-image-dropdown")
+let isopenLéo = false
+Léoimage.onclick=(event) => {
+    event.stopPropagation()
+    isopenLéo = !isopenLéo
+    const Léodropdown= document.getElementById("Léo-dropdown")
+
+    Léodropdown.style.display=isopenLéo?"block":"none"
+}
+
+const Béaimage = document.getElementById("Béa-image-dropdown")
+let isopenBéa = false
+Béaimage.onclick=(event) => {
+    event.stopPropagation()
+    isopenBéa = !isopenBéa
+    const Béadropdown= document.getElementById("Béa-dropdown")
+
+    Béadropdown.style.display=isopenBéa?"block":"none"
+}
+
+const Raymanaimage = document.getElementById("Raymana-image-dropdown")
+let isopenRaymana = false
+Raymanaimage.onclick=(event) => {
+    event.stopPropagation()
+    isopenRaymana = !isopenRaymana
+    const Raymanadropdown= document.getElementById("Raymana-dropdown")
+
+    Raymanadropdown.style.display=isopenRaymana?"block":"none"
+}
+
+const Youssefimage = document.getElementById("Youssef-image-dropdown")
+let isopenYoussef = false
+Youssefimage.onclick=(event) => {
+    event.stopPropagation()
+    isopenYoussef = !isopenYoussef
+    const Youssefdropdown= document.getElementById("Youssef-dropdown")
+
+    Youssefdropdown.style.display=isopenYoussef?"block":"none"
 }
 
 // const princesseDropdown = document.getElementById("princesse-dropdown")
