291ccc7a14b9732aca8cde128c07fb6948d5fa6a
Guillaume1234566436 <158013035+Guillaume1234566436@users.noreply.github.com>
2024-03-09 16:28:15 -0500

    Co-authored-by: ondroskotch <ondroskotch@users.noreply.github.com>

--- web/main.js
+++ web/main.js
@@ -8,11 +8,50 @@ clifimage.onclick=(event) => {
     clifdropdown.style.display=isopencliff?"block":"none"
 }
 
-const princesseDropdown = document.getElementById("princesse-dropdown")
-let isopenprincesse = false
-princesseDropdown.onclick=() => {
-    isopenprincesse = !isopenprincesse
-    const Dropdown= document.getElementById("dropdown")
+const Alexisimage = document.getElementById("Alexis-image-dropdown")
+let isopenAlexis = false
+Alexisimage.onclick=(event) => {
+    event.stopPropagation()
+    isopenAlexis = !isopenAlexis
+    const Alexisdropdown= document.getElementById("Alexis-dropdown")
 
-    Dropdown.style.display=isopenprincesse?"block":"none"
+    Alexisdropdown.style.display=isopenAlexis?"block":"none"
 }
+
+const Féliximage = document.getElementById("Félix-image-dropdown")
+let isopenFélix = false
+Féliximage.onclick=(event) => {
+    event.stopPropagation()
+    isopenFélix = !isopenFélix
+    const Félixdropdown= document.getElementById("Félix-dropdown")
+
+    Félixdropdown.style.display=isopenFélix?"block":"none"
+}
+
+const Guillaumeimage = document.getElementById("Guillaume-image-dropdown")
+let isopenGuillaume = false
+Guillaumeimage.onclick=(event) => {
+    event.stopPropagation()Guillaume
+    isopenGuillaume = !isopenGuillaume
+    const Guillaumedropdown= document.getElementById("Guillaume-dropdown")
+
+    Guillaumedropdown.style.display=isopenGuillaume?"block":"none"
+}
+
+// const princesseDropdown = document.getElementById("princesse-dropdown")
+// let isopenprincesse = false
+// princesseDropdown.onclick=() => {
+//     isopenprincesse = !isopenprincesse
+//     const Dropdown= document.getElementById("dropdown")
+
+//     Dropdown.style.display=isopenprincesse?"block":"none"
+// }
+
+// const ### = document.getElementById("###")
+// let isopen# = false
+// princesseDropdown.onclick=() => {
+//     isopen# = !isopen#
+//     const Dropdown= document.getElementById("dropdown")
+
+//     Dropdown.style.display=isopenprincesse?"block":"none"
+// }
