65715795d06018872a8ccc67865e2dc8a42ccc1e
ondroskotch <153201258+ondroskotch@users.noreply.github.com>
2024-03-17 16:41:40 -0400

    hhh

--- robot/src/BASE_ROBOT/main.cpp
+++ robot/src/BASE_ROBOT/main.cpp
@@ -1,55 +1,55 @@
-#include <CrcLib.h>
-#include <XBee.h>
-#include "CrcRemoteState.h"
+// #include <CrcLib.h>
+//  #include <XBee.h>
+//  #include "CrcRemoteState.h"
 
-// bool btnValue;
-// byte joyL = 0;
-// uint8_t JOYSTICK1_X;
+// // bool btnValue;
+// // byte joyL = 0;
+// // uint8_t JOYSTICK1_X;
 
-void setup()
-{
-  CrcLib::Initialize();
+// void setup()
+// {
+//   CrcLib::Initialize();
 
-  CrcLib::InitializePwmOutput(CRC_PWM_1);
-  CrcLib::InitializePwmOutput(CRC_PWM_2);
+//   CrcLib::InitializePwmOutput(CRC_PWM_1);
+//   CrcLib::InitializePwmOutput(CRC_PWM_2);
 
-  Serial.begin(115200); // Open the serial Monitor at a 2000000 baud rate
+//   Serial.begin(115200); // Open the serial Monitor at a 2000000 baud rate
 
-  /*
-    The rest of your setup code
-    ...
-     */
-}
-void loop()
-{
-  CrcLib::Update();
+//   /*
+//     The rest of your setup code
+//     ...
+//      */
+// }
+// void loop()
+// {
+//   CrcLib::Update();
 
-  uint8_t joy1_Y = map(CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK1_Y), -128,127,-63,63);
-  uint8_t joy1_X = map(CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK1_X), -128,127,-63,63);
+//   uint8_t joy1_Y = map(CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK1_Y), -128,127,-63,63);
+//   uint8_t joy1_X = map(CrcLib::ReadAnalogChannel(ANALOG::JOYSTICK1_X), -128,127,-63,63);
 
-  // y = map(x, 1, 50, 50, 1);
-  // map(value, fromLow, fromHigh, toLow, toHigh)
-  // JOYSTICK1_Y = map(ANALOG::JOYSTICK1_Y,-127,126,-63,63);
-  // JOYSTICK1_X = map(ANALOG::JOYSTICK1_X,-127,126,-63,63);
+//   // y = map(x, 1, 50, 50, 1);
+//   // map(value, fromLow, fromHigh, toLow, toHigh)
+//   // JOYSTICK1_Y = map(ANALOG::JOYSTICK1_Y,-127,126,-63,63);
+//   // JOYSTICK1_X = map(ANALOG::JOYSTICK1_X,-127,126,-63,63);
 
-  if (!CrcLib::IsCommValid())
-  {
-    CrcLib::MoveTank(0, 0, CRC_PWM_1, CRC_PWM_2);
+//   if (!CrcLib::IsCommValid())
+//   {
+//     CrcLib::MoveTank(0, 0, CRC_PWM_1, CRC_PWM_2);
 
-  }
-  else
-  {
-    CrcLib::MoveTank(joy1_X, joy1_Y, CRC_PWM_1, CRC_PWM_2);
-  }
-  // CrcLib::SetPwmOutput(CRC_PWM_1, joyL);
+//   }
+//   else
+//   {
+//     CrcLib::MoveTank(joy1_X, joy1_Y, CRC_PWM_1, CRC_PWM_2);
+//   }
+//   // CrcLib::SetPwmOutput(CRC_PWM_1, joyL);
 
-  // btnValue = CrcLib::ReadDigitalChannel(BUTTON::COLORS_LEFT);
-  //  Serial.print(" JOYSTICKLX : " + String(joyL) + "   ");
-  //  Serial.print("BOUTONX : " + String(btnValue));
-  //  Serial.println();
+//   // btnValue = CrcLib::ReadDigitalChannel(BUTTON::COLORS_LEFT);
+//   //  Serial.print(" JOYSTICKLX : " + String(joyL) + "   ");
+//   //  Serial.print("BOUTONX : " + String(btnValue));
+//   //  Serial.println();
 
-  /*
-    The rest of your looping code
-    ...
-    */
-}
+//   /*
+//     The rest of your looping code
+//     ...
+//     */
+// }
